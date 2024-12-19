bd32359c2cbbcd955461f98d3581091f7118d2dc
Markus-Stuppnig <markus@stuppnig.net>
2024-12-19 14:43:13 +0100

    Adds Rate limiting; Adds CORS

--- app/main.py
+++ app/main.py
@@ -5,6 +5,12 @@ from app import models
 from app.database import engine
 from app.auth import auth_controller
 
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
+limiter = Limiter(key_func=get_remote_address)
+
+
 models.Base.metadata.create_all(bind=engine)
 
 # Starting up FastAPI server
@@ -12,8 +18,14 @@ app = FastAPI()
 app.include_router(auth_controller.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Allows all origins
+    allow_origins=["http://localhost:8000"], # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type", "Autorization"],
 )
+
+
+@app.get("/")
+@limiter.limit("5/minute")  # Limit to 5 requests per minute
+async def home(request: Request):
+    return {"message": "Hello, world!"}
